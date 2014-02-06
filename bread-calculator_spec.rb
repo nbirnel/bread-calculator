df1062e353344e992291703d889dea168250a7f2
Noah Birnel <nbirnel@gmail.com>
2014-02-06 11:48:00 -0800

    pass read & write liquid_bp, fail liquid !> 100

--- bread-calculator_spec.rb
+++ bread-calculator_spec.rb
@@ -1,5 +1,25 @@
 load  "#{File.dirname(__FILE__)}/bread-calculator"
 
+describe Formula do
+  before do
+    @formula = Formula.new
+  end
+
+  it 'can write liquid_bp' do
+    @formula.liquid_bp=(35).should be_true
+  end
+
+  it 'can write and read liquid_bp' do
+    @formula.liquid_bp=35
+    @formula.liquid_bp.should eq 35
+  end
+
+  it 'will not set liquid_bp >100' do
+    @formula.liquid_bp=(101).should_not be_true
+    # what should it really be? dd
+  end
+end
+
 describe Recipe do
   before do
     @recipe = Recipe.new
@@ -7,9 +27,3 @@ describe Recipe do
 
 end
 
-describe Formula do
-  before do
-    @formula = Formula.new
-  end
-
-end
