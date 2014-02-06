0f3d80284ae491dd3e165c7f8bcdfdb87f20a360
Noah Birnel <nbirnel@gmail.com>
2014-02-06 11:07:37 -0800

    add boilerplate test and classes

---
+++ bread-calculator_spec.rb
@@ -0,0 +1,15 @@
+load  "#{File.dirname(__FILE__)}/bread-calculator"
+
+describe Recipe do
+  before do
+    @recipe = Recipe.new
+  end
+
+end
+
+describe Formula do
+  before do
+    @formula = Formula.new
+  end
+
+end
