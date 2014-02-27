2ca3b0db57bc78ec0aeb086275187a3c04377309
Noah Birnel <nbirnel@gmail.com>
2014-02-27 15:22:06 -0800

    show baker's percentage

--- bread_calculator.rb
+++ bread_calculator.rb
@@ -12,14 +12,18 @@ end
 
 class Recipe
   attr_accessor :flours, :liquids, :additives
-  def initialize
+  def initialize 
     @flours    = Hash.new(0)
     @liquids   = Hash.new(0)
     @additives = Hash.new(0)
   end
 
+  def percent_100
+    @flours.map{|k,v| v}.inject(:+).to_f
+  end
+
   def bp item
-    item / @flours.each_value{|v| v}.inject(:+)
+    item / percent_100 * 100
   end
 end
 
