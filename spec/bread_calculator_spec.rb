73aada4e82b7d39564caa3f4940552b7ff73ce2d
Noah Birnel <nbirnel@gmail.com>
2014-03-03 16:08:29 -0800

    clean up spec

--- spec/bread_calculator_spec.rb
+++ spec/bread_calculator_spec.rb
@@ -8,7 +8,7 @@ describe Ingredient do
     @ap = Ingredient.new "all purpose flour", :quantity => 700, :units => 'grams', :type =>:flour
     @water = Ingredient.new "water", :quantity => 550, :units => 'grams', :type =>:liquids
     @egg = Ingredient.new "egg", :quantity => 40, :units => 'grams', :type =>:liquids
-    @milk = Ingredient.new "milk powder", :quantity => 40, :units => 'grams', :type =>:additives
+    @milk = Ingredient.new "dry milk", :quantity => 40, :units => 'grams', :type =>:additives
     @raisins = Ingredient.new "raisins", :quantity => 50, :units => 'grams', :type =>:additives
     @yeast = Ingredient.new "yeast", :quantity => 20, :units => 'grams', :type =>:additives
   end
@@ -24,11 +24,11 @@ describe Step do
     @ap = Ingredient.new "all purpose flour", :quantity => 700, :units => 'grams', :type =>:flour
     @water = Ingredient.new "water", :quantity => 550, :units => 'grams', :type =>:liquids
     @egg = Ingredient.new "egg", :quantity => 40, :units => 'grams', :type =>:liquids
-    @milk = Ingredient.new "milk powder", :quantity => 40, :units => 'grams', :type =>:additives
+    @milk = Ingredient.new "dry milk", :quantity => 40, :units => 'grams', :type =>:additives
     @raisins = Ingredient.new "raisins", :quantity => 50, :units => 'grams', :type =>:additives
     @yeast = Ingredient.new "yeast", :quantity => 20, :units => 'grams', :type =>:additives
     @proof = Step.new 'Rehydrate', [@yeast]
-    @wet   = Step.new 'in', [@water]
+    @wet   = Step.new 'in', [@water, @egg]
     @dry   = Step.new 'Mix together:', [@ww, @ap, @milk], 'in  a large bowl'
     @mix   = Step.new 'Combine wet and dry ingredients with', [@raisins]
     @bake  = Step.new 'Form a loaf, rise for 2 hours, Bake at 375° for 45 minutes.'
@@ -46,11 +46,11 @@ describe Recipe do
     @ap = Ingredient.new "all purpose flour", :quantity => 700, :units => 'grams', :type =>:flours
     @water = Ingredient.new "water", :quantity => 550, :units => 'grams', :type =>:liquids
     @egg = Ingredient.new "egg", :quantity => 40, :units => 'grams', :type =>:liquids
-    @milk = Ingredient.new "milk powder", :quantity => 40, :units => 'grams', :type =>:additives
+    @milk = Ingredient.new "dry milk", :quantity => 40, :units => 'grams', :type =>:additives
     @raisins = Ingredient.new "raisins", :quantity => 50, :units => 'grams', :type =>:additives
     @yeast = Ingredient.new "yeast", :quantity => 20, :units => 'grams', :type =>:additives
     @proof = Step.new 'Rehydrate', [@yeast]
-    @wet   = Step.new 'in', [@water]
+    @wet   = Step.new 'in', [@water, @egg]
     @dry   = Step.new 'Mix together:', [@ww, @ap, @milk], 'in  a large bowl'
     @mix   = Step.new 'Combine wet and dry ingredients with', [@raisins]
     @bake  = Step.new 'Form a loaf, rise for 2 hours, Bake at 375° for 45 minutes.'
@@ -58,15 +58,15 @@ describe Recipe do
   end
 
   it 'lists all ingredients and quantities' do
-    @recipe.ingredients.length.should eq 6
+    @recipe.ingredients.length.should eq 7
   end
 
   it 'displays total weight' do
-    @recipe.weight.should eq 1660
+    @recipe.weight.should eq 1700
   end
 
   it 'displays total liquids' do
-    @recipe.total_liquids.should eq 550
+    @recipe.total_liquids.should eq 590
   end
 
   it 'pretty prints' do
@@ -76,12 +76,13 @@ describe Recipe do
 
   it 'generates a formula' do
     @recipe.formula.should == {
-      :whole_wheat=>0.3,
-      :all_purpose=>0.7,
-      :water=>0.55,
-      :egg=>0.04,
-      :dry_milk=>0.04,
-      :raisins=>0.05
+      "yeast"=>0.02,
+      "water"=>0.55,
+      "egg"=>0.04,
+      "whole wheat flour"=>0.3,
+      "all purpose flour"=>0.7,
+      "dry milk"=>0.04,
+      "raisins"=>0.05
     }
   end
 
