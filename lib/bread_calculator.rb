c2bf30ac8a0f8fc5c2109ad231f18b4f0034b2eb
Noah Birnel <nbirnel@gmail.com>
2014-03-07 10:21:29 -0800

    add module

--- lib/bread_calculator.rb
+++ lib/bread_calculator.rb
@@ -1,195 +1,197 @@
-##
-# This class represents an ingredient in a Recipe
-
-class Ingredient
-  attr_accessor :info, :name, :quantity, :units, :type
-
+module BreadCalculator
   ##
-  # Creates a new ingredient +name+, with the optional qualities +info+.
-  #
-  # +info+ should usually contain <tt>:quantity, :units, :type</tt>.
-  # +:type+, in the context of bakers' percentage, would be +:flours+, 
-  # +:liquids+, or +:additives+.
-  
-  def initialize name, info={}
-    @units = 'grams'
-    @name = name
-    @info = info
-    info.each do |k,v| 
-      instance_variable_set("@#{k}", v)
-    end
-  end
+  # This class represents an ingredient in a Recipe
 
-  ##
-  # Returns a new Ingredient, scaled from current instance by +ratio+
-  
-  def scale_by ratio
-    scaled = Hash.new
-    self.info.each do |k, v|
-      scaled[k] = v
-      scaled[k] = v*ratio  if k == :quantity
-    end
-    Ingredient.new(self.name, scaled)
-  end
-  
-  ##
-  # Print a nice text version of Ingredient
-  
-  def to_s
-    #FIXME check for existance
-    "\t#{@quantity} #{@units} #{@name}\n"
-  end
-end
+  class Ingredient
+    attr_accessor :info, :name, :quantity, :units, :type
 
-## 
-# This class represents a discrete step in a Recipe.
-
-class Step
-  attr_reader :techniques, :ingredients
-
-  ##
-  # Creates a new step with the the optional array +techniques+,
-  # which consists of  ministep strings, and +Ingredients+.
-  #
-  # This is intended to read something like:
-  # <tt>"Mix:", @flour, @water, "thoroughly."</tt>
-  #
-  # or:
-  #
-  # <tt>"Serve forth."</tt>
-  
-  def initialize *args
-    self.techniques = args.flatten
-  end
-
-  ##
-  # Sets +Step.techniques+ to +args+, and defines +Step.ingredients+
-  def techniques= args
-    @techniques = args
-    @ingredients = args.select{|arg| arg.is_a? Ingredient}
-  end
-  
-  ##
-  # Print a nice text version of Step
-  
-  def to_s
-    out = ''
-    self.techniques.each do |t| 
-      tmp =  t.is_a?(Ingredient) ? t.to_s : "#{t.chomp}\n"
-      out << tmp
-    end
-    out << "\n"
-    out
-  end
-end
-
-##
-# This class represents a recipe.
-
-class Recipe
-  attr_reader :steps, :metadata
-  
-  ##
-  # Creates a new Recipe with hash +metadata+ and array of Steps +steps+
-  #
-  # +metadata+ is freeform, but most likely should include +:name+.
-  # Other likely keys are: 
-  # <tt>:prep_time, :total_time, :notes, :history, :serves, :makes,
-  # :attribution</tt>.
-  
-  def initialize metadata, steps
-    @metadata = metadata
-    @steps    = steps
-    @ingredients = self.ingredients
-  end
-
-  ##
-  # Returns an array of all Ingredients in Recipe
-  
-  def ingredients
-    a = Array.new
-    self.steps.each do |step|
-      step.ingredients.each do |ing|
-        a << ing
+    ##
+    # Creates a new ingredient +name+, with the optional qualities +info+.
+    #
+    # +info+ should usually contain <tt>:quantity, :units, :type</tt>.
+    # +:type+, in the context of bakers' percentage, would be +:flours+, 
+    # +:liquids+, or +:additives+.
+    
+    def initialize name, info={}
+      @units = 'grams'
+      @name = name
+      @info = info
+      info.each do |k,v| 
+        instance_variable_set("@#{k}", v)
       end
     end
-    a
-  end
 
-  ##
-  # Returns the total weight of Ingredients in Recipe
-  
-  def weight
-    self.ingredients.map{|i| i.quantity}.reduce(:+)
-  end
-
-  #FIXME make this a method_missing so we can add new types on the fly
-  #RENÉE - 'end.' is weird or no?
-  #FIXME how do I get this into rdoc?
-  [:flours, :liquids, :additives].each do |s|
-    define_method("total_#{s}") do
-      instance_variable_get("@ingredients").select{|i| i.type == s}.map do |i|
-        i.quantity
-      end.reduce(:+)
-    end
-  end
-
-  alias_method 'bakers_percent_100', 'total_flours'
-
-  ##
-  # Returns the baker's percentage of a weight
-  
-  def bakers_percent weight
-    weight / bakers_percent_100.to_f
-  end
-
-  ##
-  # Returns a Formula
-  
-  def bakers_percent_formula
-    ratio = 100.0 / self.total_flours
-    self.scale_by ratio
-  end
-  
-  ## 
-  # Returns new Recipe scaled by +ratio+
-
-  def scale_by ratio
-    new_steps = self.steps.map do |s| 
-      step_args =  s.techniques.map do |t| 
-        t.is_a?(Ingredient) ? t.scale_by(ratio) : t
+    ##
+    # Returns a new Ingredient, scaled from current instance by +ratio+
+    
+    def scale_by ratio
+      scaled = Hash.new
+      self.info.each do |k, v|
+        scaled[k] = v
+        scaled[k] = v*ratio  if k == :quantity
       end
-      Step.new step_args
-    end
-
-    Recipe.new self.metadata, new_steps
-  end
-
-  ##
-  # Returns a unit-less summary of Recipe in baker's percentages
-  
-  def bakers_percent_summary
-    h = Hash.new
-    [:flours, :liquids, :additives].each do |s|
-      h["total_#{s}"] = self.bakers_percent eval("self.total_#{s}")
+      Ingredient.new(self.name, scaled)
     end
     
-    self.ingredients.map do |i|
-      h[i.name] = self.bakers_percent i.quantity
+    ##
+    # Print a nice text version of Ingredient
+    
+    def to_s
+      #FIXME check for existance
+      "\t#{@quantity} #{@units} #{@name}\n"
     end
-    h
+  end
+
+  ## 
+  # This class represents a discrete step in a Recipe.
+
+  class Step
+    attr_reader :techniques, :ingredients
+
+    ##
+    # Creates a new step with the the optional array +techniques+,
+    # which consists of  ministep strings, and +Ingredients+.
+    #
+    # This is intended to read something like:
+    # <tt>"Mix:", @flour, @water, "thoroughly."</tt>
+    #
+    # or:
+    #
+    # <tt>"Serve forth."</tt>
+    
+    def initialize *args
+      self.techniques = args.flatten
+    end
+
+    ##
+    # Sets +Step.techniques+ to +args+, and defines +Step.ingredients+
+    def techniques= args
+      @techniques = args
+      @ingredients = args.select{|arg| arg.is_a? Ingredient}
+    end
+    
+    ##
+    # Print a nice text version of Step
+    
+    def to_s
+      out = ''
+      self.techniques.each do |t| 
+        tmp =  t.is_a?(Ingredient) ? t.to_s : "#{t.chomp}\n"
+        out << tmp
+      end
+      out << "\n"
+      out
+    end
   end
 
   ##
-  # Print a nice text version of Recipe
-  
-  def to_s
-    out = ''
-    self.metadata.each{|k,v| out << "#{k}: #{v}\n"}
-    out << "--------------------\n"
-    self.steps.each{|s| out << s.to_s }
-    out
+  # This class represents a recipe.
+
+  class Recipe
+    attr_reader :steps, :metadata
+    
+    ##
+    # Creates a new Recipe with hash +metadata+ and array of Steps +steps+
+    #
+    # +metadata+ is freeform, but most likely should include +:name+.
+    # Other likely keys are: 
+    # <tt>:prep_time, :total_time, :notes, :history, :serves, :makes,
+    # :attribution</tt>.
+    
+    def initialize metadata, steps
+      @metadata = metadata
+      @steps    = steps
+      @ingredients = self.ingredients
+    end
+
+    ##
+    # Returns an array of all Ingredients in Recipe
+    
+    def ingredients
+      a = Array.new
+      self.steps.each do |step|
+        step.ingredients.each do |ing|
+          a << ing
+        end
+      end
+      a
+    end
+
+    ##
+    # Returns the total weight of Ingredients in Recipe
+    
+    def weight
+      self.ingredients.map{|i| i.quantity}.reduce(:+)
+    end
+
+    #FIXME make this a method_missing so we can add new types on the fly
+    #RENÉE - 'end.' is weird or no?
+    #FIXME how do I get this into rdoc?
+    [:flours, :liquids, :additives].each do |s|
+      define_method("total_#{s}") do
+        instance_variable_get("@ingredients").select{|i| i.type == s}.map do |i|
+          i.quantity
+        end.reduce(:+)
+      end
+    end
+
+    alias_method 'bakers_percent_100', 'total_flours'
+
+    ##
+    # Returns the baker's percentage of a weight
+    
+    def bakers_percent weight
+      weight / bakers_percent_100.to_f
+    end
+
+    ##
+    # Returns a Formula
+    
+    def bakers_percent_formula
+      ratio = 100.0 / self.total_flours
+      self.scale_by ratio
+    end
+    
+    ## 
+    # Returns new Recipe scaled by +ratio+
+
+    def scale_by ratio
+      new_steps = self.steps.map do |s| 
+        step_args =  s.techniques.map do |t| 
+          t.is_a?(Ingredient) ? t.scale_by(ratio) : t
+        end
+        Step.new step_args
+      end
+
+      Recipe.new self.metadata, new_steps
+    end
+
+    ##
+    # Returns a unit-less summary of Recipe in baker's percentages
+    
+    def bakers_percent_summary
+      h = Hash.new
+      [:flours, :liquids, :additives].each do |s|
+        h["total_#{s}"] = self.bakers_percent eval("self.total_#{s}")
+      end
+      
+      self.ingredients.map do |i|
+        h[i.name] = self.bakers_percent i.quantity
+      end
+      h
+    end
+
+    ##
+    # Print a nice text version of Recipe
+    
+    def to_s
+      out = ''
+      self.metadata.each{|k,v| out << "#{k}: #{v}\n"}
+      out << "--------------------\n"
+      self.steps.each{|s| out << s.to_s }
+      out
+    end
+
   end
 
 end
-
--- spec/bread_calculator_spec.rb
+++ spec/bread_calculator_spec.rb
@@ -1,36 +1,36 @@
 require  "#{File.dirname(__FILE__)}/../lib/bread_calculator"
 
-describe 'bread_calculator' do
+describe BreadCalculator do
   before do
-    @ww      = Ingredient.new "whole wheat flour", :quantity => 300, :units => 'grams', :type=>:flours
-    @ap      = Ingredient.new "all purpose flour", :quantity => 700, :units => 'grams', :type=>:flours
-    @water   = Ingredient.new "water", :quantity => 550, :units => 'grams', :type=>:liquids
-    @egg     = Ingredient.new "egg", :quantity => 40, :units => 'grams', :type=>:liquids
-    @milk    = Ingredient.new "dry milk", :quantity => 40, :units => 'grams', :type=>:additives
-    @raisins = Ingredient.new "raisins", :quantity => 50, :units => 'grams', :type=>:additives
-    @yeast   = Ingredient.new "yeast", :quantity => 20, :units => 'grams', :type=>:additives
-    @proof   = Step.new 'Rehydrate', @yeast
-    @wet     = Step.new 'in', @water, @egg
-    @dry     = Step.new 'Mix together:', @ww, @ap, @milk, 'in  a large bowl'
-    @mix     = Step.new 'Combine wet and dry ingredients with', @raisins
-    @bake    = Step.new 'Form a loaf, rise for 2 hours, Bake at 375° for 45 minutes.'
+    @ww      = BreadCalculator::Ingredient.new "whole wheat flour", :quantity => 300, :units => 'grams', :type=>:flours
+    @ap      = BreadCalculator::Ingredient.new "all purpose flour", :quantity => 700, :units => 'grams', :type=>:flours
+    @water   = BreadCalculator::Ingredient.new "water", :quantity => 550, :units => 'grams', :type=>:liquids
+    @egg     = BreadCalculator::Ingredient.new "egg", :quantity => 40, :units => 'grams', :type=>:liquids
+    @milk    = BreadCalculator::Ingredient.new "dry milk", :quantity => 40, :units => 'grams', :type=>:additives
+    @raisins = BreadCalculator::Ingredient.new "raisins", :quantity => 50, :units => 'grams', :type=>:additives
+    @yeast   = BreadCalculator::Ingredient.new "yeast", :quantity => 20, :units => 'grams', :type=>:additives
+    @proof   = BreadCalculator::Step.new 'Rehydrate', @yeast
+    @wet     = BreadCalculator::Step.new 'in', @water, @egg
+    @dry     = BreadCalculator::Step.new 'Mix together:', @ww, @ap, @milk, 'in  a large bowl'
+    @mix     = BreadCalculator::Step.new 'Combine wet and dry ingredients with', @raisins
+    @bake    = BreadCalculator::Step.new 'Form a loaf, rise for 2 hours, Bake at 375° for 45 minutes.'
     @meta    = {:notes => 'nice sandwich bread'}
-    @recipe  = Recipe.new @meta, [@proof, @wet, @dry, @mix, @bake]
+    @recipe  = BreadCalculator::Recipe.new @meta, [@proof, @wet, @dry, @mix, @bake]
   end
 
-  describe Ingredient do
+  describe BreadCalculator::Ingredient do
     it 'has a quantity' do
       @ww.quantity = 100
     end
   end
 
-  describe Step do
+  describe BreadCalculator::Step do
     it 'can be called without arguments' do
-      Step.new
+      BreadCalculator::Step.new
     end
   end
 
-  describe Recipe do
+  describe BreadCalculator::Recipe do
     it 'lists all ingredients and quantities' do
       @recipe.ingredients.length.should eq 7
     end
@@ -70,7 +70,7 @@ describe 'bread_calculator' do
     end
 
     it 'generates a baker\'s percentage formula' do
-      @recipe.bakers_percent_formula.is_a?(Recipe).should be_true
+      @recipe.bakers_percent_formula.is_a?(BreadCalculator::Recipe).should be_true
       @recipe.bakers_percent_formula.total_flours.should eq 100.0
     end
 
